a2304faf46dca8a5626b50d03cc5d5e921416a1b
Usama Tahir <utahir@gosaas.io>
2020-05-02 04:47:30 +0500

    Improve leetcode solution for valid mountain array in python

--- datastructure/arrays/easy/valid-mountain-array.py
+++ datastructure/arrays/easy/valid-mountain-array.py
@@ -9,28 +9,26 @@ class Solution:
         print('max value in arr', max_value)
         max_index = A.index(max_value)
         print('max value index: ', max_index)
-        # split A in two sub arrays.
-        arr_one = A[:max_index]
-        arr_two = A[max_index + 1:]
+        previousValue = None
         validMountainArr = True
-
-        if (len(arr_one) == 0 or len(arr_two) == 0):
+        if A[-1] == max_value or A[0] == max_value:
             return False
-        if (arr_one[0] == max_value or arr_two[0] == max_value):
-            return False
-        previousValue = arr_one[0]
-        for val in islice(arr_one, 1, None):
-            if val <= previousValue:
-                return False
-            previousValue = val
-        previousValue = arr_two[0]
-        for val in islice(arr_two, 1, None):
-            if val >= previousValue:
-                return False
+        for i, val in enumerate(A):
+            if i == 0:
+                previousValue = val
+                continue
+            if i <= max_index:
+                if val <= previousValue:
+                    validMountainArr = False
+                    break
+            if i > max_index:
+                if (val >= previousValue):
+                    validMountainArr = False
+                    break
             previousValue = val
         return validMountainArr
 
 
 mountain = Solution()
 # print(mountain.validMountainArray([1,2,3,4,5,4,3,2,1]))
-print(mountain.validMountainArray([2,1,2]))
+print(mountain.validMountainArray([0,1,2,3,4,5,6,7,8,9]))
